31991b48fa075420509f03a3dd24eb27f4247749
AndrewWalker251 <andrewjwalker251@gmail.com>
2020-05-18 18:35:42 +0000

    Controller completed to follow waymarkers using PID

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -51,9 +51,9 @@ class WaypointUpdater(object):
         #rospy.spin()
        
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(25)
         while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints:
+            if (self.pose and self.base_waypoints and self.waypoint_tree):
                 closest_waypoint_idx = self.get_closest_waypoint_id()
                 self.publish_waypoints(closest_waypoint_idx)
             rate.sleep()
